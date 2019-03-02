a7b7ae057b6ffbe42d425bc900459938030dfbd9
dd84ai <dd84ai@gmail.com>
2019-03-02 16:58:59 +0700

    InverseMatrix

--- Lab1TimeSeries/Lab1TimeSeries/Program.cs
+++ Lab1TimeSeries/Lab1TimeSeries/Program.cs
@@ -11,6 +11,174 @@ namespace Lab1TimeSeries
 
         public class Matrix
         {
+
+            Tuple<double[][], int[]> LUPDecomposition(double[][] A)
+            {
+                int n = A.Length - 1;
+                /*
+                * pi represents the permutation matrix.  We implement it as an array
+                * whose value indicates which column the 1 would appear.  We use it to avoid 
+                * dividing by zero or small numbers.
+                * */
+                int[] pi = new int[n + 1];
+                double p = 0;
+                int kp = 0;
+                int pik = 0;
+                int pikp = 0;
+                double aki = 0;
+                double akpi = 0;
+
+                //Initialize the permutation matrix, will be the identity matrix
+                for (int j = 0; j <= n; j++)
+                {
+                    pi[j] = j;
+                }
+
+                for (int k = 0; k <= n; k++)
+                {
+                    /*
+                    * In finding the permutation matrix p that avoids dividing by zero
+                    * we take a slightly different approach.  For numerical stability
+                    * We find the element with the largest 
+                    * absolute value of those in the current first column (column k).  If all elements in
+                    * the current first column are zero then the matrix is singluar and throw an
+                    * error.
+                    * */
+                    p = 0;
+                    for (int i = k; i <= n; i++)
+                    {
+                        if (Math.Abs(A[i][k]) > p)
+                        {
+                            p = Math.Abs(A[i][k]);
+                            kp = i;
+                        }
+                    }
+                    if (p == 0)
+                    {
+                        throw new Exception("singular matrix");
+                    }
+                    /*
+                    * These lines update the pivot array (which represents the pivot matrix)
+                    * by exchanging pi[k] and pi[kp].
+                    * */
+                    pik = pi[k];
+                    pikp = pi[kp];
+                    pi[k] = pikp;
+                    pi[kp] = pik;
+
+                    /*
+                    * Exchange rows k and kpi as determined by the pivot
+                    * */
+                    for (int i = 0; i <= n; i++)
+                    {
+                        aki = A[k][i];
+                        akpi = A[kp][i];
+                        A[k][i] = akpi;
+                        A[kp][i] = aki;
+                    }
+
+                    /*
+                        * Compute the Schur complement
+                        * */
+                    for (int i = k + 1; i <= n; i++)
+                    {
+                        A[i][k] = A[i][k] / A[k][k];
+                        for (int j = k + 1; j <= n; j++)
+                        {
+                            A[i][j] = A[i][j] - (A[i][k] * A[k][j]);
+                        }
+                    }
+                }
+                return Tuple.Create(A, pi);
+            }
+            double[] LUPSolve(double[][] LU, int[] pi, double[] b)
+            {
+                int n = LU.Length - 1;
+                double[] x = new double[n + 1];
+                double[] y = new double[n + 1];
+                double suml = 0;
+                double sumu = 0;
+                double lij = 0;
+
+                /*
+                * Solve for y using formward substitution
+                * */
+                for (int i = 0; i <= n; i++)
+                {
+                    suml = 0;
+                    for (int j = 0; j <= i - 1; j++)
+                    {
+                        /*
+                        * Since we've taken L and U as a singular matrix as an input
+                        * the value for L at index i and j will be 1 when i equals j, not LU[i][j], since
+                        * the diagonal values are all 1 for L.
+                        * */
+                        if (i == j)
+                        {
+                            lij = 1;
+                        }
+                        else
+                        {
+                            lij = LU[i][j];
+                        }
+                        suml = suml + (lij * y[j]);
+                    }
+                    y[i] = b[pi[i]] - suml;
+                }
+                //Solve for x by using back substitution
+                for (int i = n; i >= 0; i--)
+                {
+                    sumu = 0;
+                    for (int j = i + 1; j <= n; j++)
+                    {
+                        sumu = sumu + (LU[i][j] * x[j]);
+                    }
+                    x[i] = (y[i] - sumu) / LU[i][i];
+                }
+                return x;
+            }
+            double[][] InvertMatrix(double[][] A)
+            {
+                int n = A.Length;
+                //e will represent each column in the identity matrix
+                double[] e;
+                //x will hold the inverse matrix to be returned
+                double[][] x = new double[n][];
+                for (int i = 0; i < n; i++)
+                {
+                    x[i] = new double[A[i].Length];
+                }
+                /*
+                * solve will contain the vector solution for the LUP decomposition as we solve
+                * for each vector of x.  We will combine the solutions into the double[][] array x.
+                * */
+                double[] solve;
+
+                //Get the LU matrix and P matrix (as an array)
+                Tuple<double[][], int[]> results = LUPDecomposition(A);
+
+                double[][] LU = results.Item1;
+                int[] P = results.Item2;
+
+                /*
+                * Solve AX = e for each column ei of the identity matrix using LUP decomposition
+                * */
+                for (int i = 0; i < n; i++)
+                {
+                    e = new double[A[i].Length];
+                    e[i] = 1;
+                    solve = LUPSolve(LU, P, e);
+                    for (int j = 0; j < solve.Length; j++)
+                    {
+                        x[j][i] = solve[j];
+                    }
+                }
+                return x;
+            }
+            public Matrix ToInverse()
+            {
+                return new Matrix(InvertMatrix(this.ToArray()));
+            }
             //Every list is a column of matrix;
             List<List<double>> m;
             public Matrix()
@@ -21,6 +189,28 @@ namespace Lab1TimeSeries
             public Matrix(List<List<double>> inp)
             {
                 m = inp;
+            }
+            public double[][] ToArray()
+            {
+                double[][] temp = new double[Rows()][];
+                for (int i = 0; i < Rows(); i++)
+                    temp[i] = m[i].ToArray();
+                return temp;
+            }
+            public Matrix ToTranspose()
+            {
+                var temp = new Matrix(Nulificator(Columns(),Rows()));
+
+                for (int i = 0; i < Rows(); i++)
+                {
+                    for (int j = 0; j < Columns(); j++)
+                    {
+                        temp.m[j][i] = m[i][j];
+
+                    }
+                }
+
+                return temp;
             }
             public Matrix(double[,] inp)
             {
@@ -33,6 +223,21 @@ namespace Lab1TimeSeries
                     for (int j = 0; j < r; j++)
                     {
                         m[i][j] = inp[i, j];
+
+                    }
+                }
+            }
+            public Matrix(double[][] inp)
+            {
+                int c = inp.GetLength(0); //Column Length
+                int r = inp[0].GetLength(0); //Row Length
+                m = Nulificator(c, r);
+
+                for (int i = 0; i < c; i++)
+                {
+                    for (int j = 0; j < r; j++)
+                    {
+                        m[i][j] = inp[i][j];
 
                     }
                 }
@@ -97,7 +302,7 @@ namespace Lab1TimeSeries
         {
             Matrix a = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 7, 8, 9 } });
             Matrix b = new Matrix(new double[,] { { 1 }, { 2 }, { 3 } });
-            Matrix result = a * b;
+            Matrix result = (a.ToTranspose() * a).ToInverse()*a.ToTranspose();//a * b;
             Console.WriteLine(result.ToString());
             Console.ReadLine();
         }
